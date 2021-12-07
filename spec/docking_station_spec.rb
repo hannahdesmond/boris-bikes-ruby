0dd3532f59949880597fda68069db6f98624fba9
Hannah Desmond <desmond.hannah@googlemail.com>
2021-12-07 17:23:15 +0000

    Add 'returns new bike' expectation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,9 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+  it 'returns a new bike' do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    expect(docking_station.release_bike).to eq(bike)
+  end
 end
