12d77af7a46dff1e83da7d13271a5d8c5b56b287
Hannah Desmond <desmond.hannah@googlemail.com>
2021-12-08 19:30:31 +0000

    update tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,9 +2,17 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bike
-  
-  def release_bike(bike)
-    bike
+
+  def release_bike
+    if @bike == nil 
+      puts "I am in the if clause"
+      bike = Bike.new 
+      puts "I am the new #{bike}"
+    else 
+      puts "I am in the else clause"
+      raise("There are no bikes available")
+      puts "I am after the exception"
+    end
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,25 +2,22 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
-  it 'returns a new bike' do
-    docking_station = DockingStation.new
-    bike = Bike.new
-    expect(docking_station.release_bike(bike)).to eq(bike)
-  end
-  it 'returns a working bike' do
-    docking_station = DockingStation.new
-    bike = Bike.new
-    expect(bike.working?).to eq(true)
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
   it 'docks a bike' do
-    docking_station = DockingStation.new
-    bike = Bike.new
-    expect(docking_station.dock(bike)).to eq(bike)
+    bike = subject.release_bike
+    expect(subject.dock(bike)).to eq(bike)
   end
   it 'shows docked bikes' do
     docking_station = DockingStation.new
     bike = Bike.new
     docking_station.dock(bike)
     expect(docking_station.bike).to eq(bike)
+  end
+  it 'throws an error if there are no bikes' do
+    bike = subject.release_bike
+    expect { subject.release_bike }.to raise_error("There are no bikes available")
   end
 end
