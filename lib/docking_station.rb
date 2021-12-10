37a285d0f788c8beab8bec667d08a6bafb35dbda
Hannah Desmond <desmond.hannah@googlemail.com>
2021-12-10 11:39:33 +0000

    Raise exception if docking station is full or has no bikes to release

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,15 +4,19 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    if @bike != nil 
-      raise("There are no bikes available")
-    else 
+    if @bike == nil 
       @bike = Bike.new 
+    else 
+      raise("There are no bikes available")
     end
   end
 
   def dock(bike)
-    @bike = bike
+    if @bike != nil 
+      raise("The docking station is full")
+    else
+      @bike = bike
+    end
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,8 +7,9 @@ describe DockingStation do
     expect(bike).to be_working
   end
   it 'docks a bike' do
-    bike = subject.release_bike
-    expect(subject.dock(bike)).to eq(bike)
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq(bike)
   end
   it 'shows docked bikes' do
     docking_station = DockingStation.new
@@ -16,8 +17,13 @@ describe DockingStation do
     docking_station.dock(bike)
     expect(docking_station.bike).to eq(bike)
   end
-  it 'throws an error if there are no bikes' do
+  it 'throws an error for #release_bike if there are no bikes' do
     bike = subject.release_bike
     expect { subject.release_bike }.to raise_error("There are no bikes available")
+  end
+  it 'throws an error for #dock(bike) if the docking station is full' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect { subject.dock(bike).to raise_error("The docking station is full") }
   end
 end
