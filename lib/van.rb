16dcb3e144d7c596cc826fefa9c46faf95d909ba
Hannah Desmond <desmond.hannah@googlemail.com>
2021-12-21 10:56:28 +0000

    Add collect method to van class

--- lib/van.rb
+++ lib/van.rb
@@ -1,7 +1,19 @@
+require_relative 'bike'
+require_relative 'docking_station'
+
 class Van
   attr_reader :bikes
 
   def initialize
     @bikes = []
+  end
+
+  def collect(bikes)
+    bikes.map! do |bike| 
+      if bike.broken == true
+        @bikes << bike
+      end
+    end
+    
   end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,19 +1,19 @@
 require 'van'
+require 'bike'
 
 describe Van do
-  let(:bike) { double :bike }
-  let(:docking_station) { double :docking_station }
-  before do
-    allow(bike).to receive(:broken).and_return(true)
-    # allow(docking_station).to receive(:bikes).and_return([bike])
-  end
+  let(:bike) { instance_double(Bike, broken: false) }
+  let(:broken_bike) { instance_double(Bike, broken: true) }
+  # add them to a bike array?
+  let(:docking_station) { instance_double(DockingStation, bikes: [bike, broken_bike, bike]) }
   it 'stores bikes in the van on the way to the station' do
       expect(subject.bikes).to eq([])
   end
   it 'collects broken bikes from docking stations' do
-    bike.broken
-    van.collect(broken_bikes)
-    expect(van.bikes) to include(bike)
+    subject.collect(docking_station.bikes)
+    expect(subject.bikes).to include(broken_bike)
+    expect(subject.bikes).not_to include(bike)
+    expect(docking_station.bikes).not_to include(broken_bike)
   end
   it 'delivers broken bikes to garages' do
 
